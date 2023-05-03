76944ade4d1d04d708df8555310d23dc6a128ae6
Monika Stokowska <monika_stokowska@tlen.pl>
2023-05-03 09:40:15 +0200

    Added toDOList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,24 @@ import { Component } from '@angular/core';
 
     <app-input-button-unit></app-input-button-unit>
 
+    <ul>
+      <li *ngFor = "let dietItem of dietApp">
+      {{ dietItem.title}}
+    </ul>
+
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-diet-app';
+  dietApp = [
+    {title: 'take vitamin B12'},
+    {title: 'take magnesium'},
+    {title: 'take anti-allergy pill'},
+    {title: 'take vitamin H'},
+    {title: 'take sodium butyrate'},
+    {title: 'take Ashwaganda'},
+    {title: 'take Omega-3 fatty acids '},
+  ];
 }
+
